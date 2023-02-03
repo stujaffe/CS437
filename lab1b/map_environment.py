6d1ebdbe19987c1570dfce165b8a172f9fa5e441
stujaffe <stuart.jaffe@gmail.com>
2023-02-03 18:22:09 -0500

    add things to a class for the car

--- lab1b/map_environment.py
+++ lab1b/map_environment.py
@@ -12,21 +12,196 @@ X_LOCAL = 100
 Y_LOCAL = 100
 X_GLOBAL = 1000
 Y_GLOBAL = 1000
-ANGLE_RANGE = 140
-STEP = 14
-CURRENT_ANGLE = 0
-POWER = 20
-SPEED_AT_10 = 26.70
-#################################
-# Calculated global vars
-MAX_ANGLE = ANGLE_RANGE/2
-MIN_ANGLE = ANGLE_RANGE/2*-1
 LOCAL_MAP = np.zeros(shape=(X_LOCAL,Y_LOCAL))
 GLOBAL_MAP = np.zeros(shape=(X_GLOBAL, Y_GLOBAL))
 #################################
-# Dictionary to keep track of the number of seconds traveled
-# in each direction
-DIRECTION_TIME = {"north":0,"east":0,"west":0,"south":0}
+
+class PiCar:
+    def __init__(self, start_loc: Coordinate, end_loc: Coordinate, power: int = 10,
+        , direction: str ='N', angle_range: int = 140) -> None:
+        self.start_loc = start_loc
+        self.goal_loc = goal_loc
+        self.current_loc = start_loc
+        self.direction = direction
+        self.distance_traveled = 0
+        self.power = power
+        self.angle_range = angle_range
+        self.step = math.floor(angle_range/10)
+        self.max_angle = angle_range/2
+        self.min_angle = angle_ange/2*-1
+        self.current_angle = 0
+        self.speed_at_10 = 26.70
+    
+    
+    # calculate the manhattan distance between two coordinates
+    @staticmethod
+    def calc_dist_mhtn(coord1: Coordinate, coord2: Coordinate) -> int:
+        distance = abs(coord2.x - coord1.x) + abs(coord2.y - coord1.y)
+        return distance
+        
+    # calculate the speed in cm/s for a given power
+    def get_speed(self):
+        if self.power == 10:
+            return self.speed_at_10
+        # extra speed is the power above 10 subtracted by 10
+        # then divided by 10, multiplied by 2.5
+        # During test I found for every 10 extra power above 10,
+        # the car moved 2.5 cm/s faster. E.g. at 20 power the speed
+        # is 26.7 + 2.5
+        extra = (self.power - 10)/10*2.5
+        self.speed = self.speed_at_10 + extra
+        
+        return self.speed
+    
+    # return the number of seconds needed to move along one axis (x or y) and the distance
+    # given the current power and the two coordinates
+    def calc_sec_dist(self, coord1: Coordinate, coord2: Coordinate, axis: str) -> Tuple[float, float]:
+        axis = str(axis).lower()
+        if axis not in ["x","y"]:
+            raise Exception("Please specify axis on which to calculate seconds to move.")
+        
+        if axis == "x":
+            dist_cm = abs(coord1.x - coord2.x)
+        elif axis == "y":
+            dist_cm = abs(coord1.y - coord2.y)
+        else:
+            dist_cm = 0
+        
+        seconds = dist_cm/self.get_speed(self.power)
+        
+        return dist_cm, seconds
+    
+    # scan the area in front of the car
+    def scan_sweep(self) -> List[Tuple[float, int]]:
+        scan_result = []
+        for angle in range(self.min_angle, self.max_angle, self.step):
+            fc.servo.set_angle(angle)
+            self.current_angle = angle
+            time.sleep(0.04)
+            distance_to_obj = fc.us.get_distance()
+            scan_result.append((distance_to_obj, angle))
+        # returns a list of tuples (distance cm, angle degrees)
+        return scan_result
+    
+    # convert angle and distance into caretisan coordinates
+    @staticmethod
+    def get_cartesian(angle: float, distance: float) -> Coordinate:
+        # x can be negative (since the car is in the middle of the grid along the x plane)
+        x = math.floor(math.sin(math.radians(angle))*distance)
+        # y will always be >= 0 (since the car cannot detect objects behind with the servo and ultrasonic sensor)
+        y = math.floor(math.cos(math.radians(angle))*distance)
+        coord = Coordinate(x_cord, y_cord)
+
+        return coord
+    
+    # calculate slope between two x,y points
+    @staticmethod
+    def get_slope(coord1: Coordinate, coord2: Coordinate) -> float:
+        if abs(coord2.x - coord1.x) > 0:
+            slope = (y2 - y1)/(x2 - x1)
+        # account for infinite slope
+        else:
+            slope = float(-999)
+        
+        return slope
+    
+    # get all x,y points between two x,y points
+    def get_points_inbtw(self, coord1: Coordinate, coord2: Coordinate) -> List[Coordinate]:
+        points_inbtw = []
+        # first get the slope
+        slope = self.get_slope(coord1, coord2)
+        # we know the equation of the line between two points is y=mx+b, where b is the y intercept
+        # calculate the y intercept using either x,y pair
+        b = coord2.y - slope*coord1.x
+        if b == -999:
+            return points_inbtw
+        # now loop from the lower to higher x coordinate and get the x,y points in between
+        if coord1.x > coord2.x:
+            x_high = coord1.x
+            x_low = coord2.x
+        else:
+            x_high = coord2.x
+            x_low = coord1.x
+        
+        for x in range(x_low, x_high):
+            y_calc = slope*x + b
+            point = Coordinate(x, y_calc)
+            points_inbtw.append(point)
+        
+        return points_inbtw
+    
+    def move_forward(self, distance, seconds):
+        # move the car backwards for number of seconds based on the result
+        # of self.calc_sec_dist()
+        fc.forward(self.power)
+        time.sleep(seconds)
+        # save the current location
+        prev_loc = self.current_loc
+        # update the current location in the map
+        if self.direction == 'N':
+            self.current_loc.y += distance
+        elif self.direction == 'E':
+            self.current_loc.x += distance
+        elif self.direction == 'S':
+            self.current_loc.y -= distance
+        elif self.direction == 'W':
+            self.current_loc.x -= distance
+        
+        # calculate the total manhattan distance traveled
+        # Note: this is not used to find where the car is on the map
+        # but could be useful for some other purpose, so let's keep it for now
+        self.distance_traveled += self.calc_dist_mhtn(prev_loc, self.current_loc)
+    
+    def move_backward(self, distance, seconds):
+        # move the car backwards for number of seconds based on the result
+        # of self.calc_sec_dist()
+        fc.backward(self.power)
+        time.sleep(seconds)
+        # save the current location
+        prev_loc = self.current_loc
+        # update the current location in the map
+        if self.direction == 'N':
+            self.current_loc.y -= distance
+        elif self.direction == 'E':
+            self.current_loc.x -= distance
+        elif self.direction == 'S':
+            self.current_loc.y += distance
+        elif self.direction == 'W':
+            self.current_loc.x += distance
+        
+        # calculate the total manhattan distance traveled
+        # Note: this is not used to find where the car is on the map
+        # but could be useful for some other purpose, so let's keep it for now
+        self.distance_traveled += self.calc_dist_mhtn(prev_loc, self.current_loc)
+    
+    def turn_left_90(self):
+        # based on experimentation to get a 90 degree turn
+        fc.turn_left(30)
+        time.sleep(0.95)
+        # update the car's absolute direction
+        if self.direction == 'N':
+            self.direction = 'W'
+        elif self.direction == 'E':
+            self.direction = 'N'
+        elif self.direction == 'S':
+            self.direction = 'E'
+        elif self.direction == 'W':
+            self.direction = 'S'
+    
+    def turn_right_90(self):
+        # based on experimentation to get a 90 degree turn
+        fc.turn_right(30)
+        time.sleep(0.95)
+        # update the car's absolute direction
+        if self.direction == 'N':
+            self.direction = 'E'
+        elif self.direction == 'E':
+            self.direction = 'S'
+        elif self.direction == 'S':
+            self.direction = 'W'
+        elif self.direction == 'W':
+            self.direction = 'N'
+
 
 class Coordinate(object):
     def __init__(self, x: int, y: int) -> None:
@@ -45,90 +220,9 @@ class Coordinate(object):
         new_point = Coordinate(self.x + other.x, self.y + other.y)
         return new_point
 
-# convert angle and distance into caretisan coordinates
-def get_cartesian(angle: float, distance: float) -> Coordinate:
-    # x can be negative (since the car is in the middle of the grid along the x plane)
-    x = math.floor(math.sin(math.radians(angle))*distance)
-    # y will always be >= 0 (since the car cannot detect objects behind with the servo and ultrasonic sensor)
-    y = math.floor(math.cos(math.radians(angle))*distance)
-    coord = Coordinate(x_cord, y_cord)
 
-    return coord
-
-# calculate slope between two x,y points
-def get_slope(coord1: Coordinate, coord2: Coordinate) -> float:
-    if coord2.x - coord1.x > 0:
-        slope = (y2 - y1)/(x2 - x1)
-    # account for infinite slope
-    else:
-        slope = float(-999)
     
-    return slope
-
-# get all x,y points between two x,y points
-def get_points_inbtw(coord1: Coordinate, coord2: Coordinate) -> List[Coordinate]:
-    points_inbtw = []
-    # first get the slope
-    slope = get_slope(coord1, coord2)
-    # we know the equation of the line between two points is y=mx+b, where b is the y intercept
-    # calculate the y intercept using either x,y pair
-    b = coord2.y - slope*coord1.x
-    # now loop from the lower to higher x coordinate and get the x,y points in between
-    if coord1.x > coord2.x:
-        x_high = coord1.x
-        x_low = coord2.x
-    else:
-        x_high = coord2.x
-        x_low = coord1.x
-    
-    for x in range(x_low, x_high):
-        y_calc = slope*x + b
-        point = Coordinate(x, y_calc)
-        points_inbtw.append(point)
-    
-    return poinst_inbtw
-
-# scan the area in front of the car
-def scan_sweep() -> List[Tuple[float, int]]:
-    
-    scan_result = []
-    for angle in range(MIN_ANGLE, MAX_ANGLE, STEP):
-        fc.servo.set_angle(angle)
-        CURRENT_ANGLE = angle
-        time.sleep(0.04)
-        distance_to_obj = fc.us.get_distance()
-        scan_result.append((distance_to_obj, angle))
-    # returns a list of tuples (distance cm, angle degrees)
-    return scan_result
-
-# calculate the speed in cm/s for a given power
-def get_speed(power: int) -> float:
-    speed_10 = SPEED_AT_10
-    if power == 10:
-        return speed_10
-    # extra speed is the power above 10 subtracted by 10
-    # then divided by 10, multiplied by 2.5
-    # During test I found for every 10 extra power above 10,
-    # the car moved 2.5 cm/s faster. E.g. at 20 power the speed
-    # is 26.7 + 2.5
-    extra = (power - 10)/10*2.5
-    speed = speed_10 + extra
-    
-    return speed
-
-def right_turn_90() -> None:
-    fc.turn_right(30)
-    time.sleep(0.95)
-
-def left_turn_90() -> None:
-    fc.turn_left(30)
-    time.sleep(0.95)
-
-# returns the location of the car in the global map
-def get_global_location(start: Coordinate) -> Coordinate:
-    pass
-    
-
+"""
 def navigate_to_goal(start: Coordinate, goal: Coordinate) -> None:
     x_cm = goal.x - start.x
     y_cm = goal.y - start.y
@@ -165,14 +259,10 @@ def navigate_to_goal(start: Coordinate, goal: Coordinate) -> None:
         fc.stop()
 
     fc.stop()
-
+"""
 
 if __name__ == "__main__":
-    
-    start = Coordinate(50,50)
-    goal = Coordinate(25, 100)
-    
-    navigate_to_goal(start, goal)
+    pass
     
     
 
